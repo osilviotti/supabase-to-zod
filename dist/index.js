c750507ec9610f46d3ca162ce104b1346f9be501
Ollie Silviotti <osilviotti@gmail.com>
2024-02-08 16:20:09 +0000

    Adds built library files

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import {
+  supabaseToZod,
+  transformTypes
+} from "./chunk-DCTSIM2S.js";
+
+// src/index.ts
+var src_default = supabaseToZod;
+export {
+  src_default as default,
+  transformTypes
+};
+//# sourceMappingURL=index.js.map
